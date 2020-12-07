294658c7c7f3deb190ee1b8ac863c3dae956826a
Kayode <adetkay@gmail.com>
2020-12-06 23:31:32 -0500

    Update daysuntilevent.py
    Complete

--- daysuntilevent.py
+++ daysuntilevent.py
@@ -1,7 +1,7 @@
-##How many days are there until?
+# How many days are there until?
 # x event( Birthdays? and others)
 
-#import b/c we are working with dates
+# import b/c we are working with dates
 import datetime
 
 
@@ -16,23 +16,42 @@ print()
 # getting user input for birthday , x , event
 
 def get_birthday():
-    print ("When were you born?", end="")
+    print("When were you born?", end="")
     year = int(input("What year were you born in?:"))
     month = int(input("What month were you born in?:"))
     day = int(input("What day were you born in?:"))
 
+    birthdate = datetime.date(year, month, day)
+    return birthdate
 
-    birthdate = datetime.date(year,month,day)
-    return birthdate 
-#calculating 
+# calculating
+
+
+def compare(exisitingdate, currentdate):
+    currentyear = datetime.date(
+        currentdate.year, currentdate.month, currentdate.day)
+    dt = exisitingdate - currentdate
+    return dt.days
 
 # output to screen
 
-#define a main method
+
+def printoutput(days):
+    if days < 0:
+        print("You had your birthday already")
+    elif days > 0:
+        print("You birthday is in {} days".format(days))
+    else:
+        print("Happy Birthday!!")
+
+
+# define a main method
 
 def main():
-    header()
-
+   # header()
+    existingdate = get_birthday()
+    today = datetime.date.today
+    print(finaldate)
 
 main()
 finaldate = get_birthday()
